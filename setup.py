52b9bfe40576f5414050384ea13bebaf7514bbe2
Emmanuel Goh <emmanuel@cynopsis-solutions.com>
2017-11-07 11:07:38 +0800

    Fix dependencies

--- setup.py
+++ setup.py
@@ -7,10 +7,13 @@ from setuptools import setup
 
 setup(
     name="json_obj",
-    version="2.0.0",
+    version="2.0.1",
     description="Safe JSON decoding to objects in Python",
     author="Emmanuel Goh",
     author_email="emmanuel.goh.7@gmail.com",
     license="MIT",
-    url="https://github.com/emman27/json_obj"
+    url="https://github.com/emman27/json_obj",
+    install_requires=[
+        'python-dateutil'
+    ]
 )
